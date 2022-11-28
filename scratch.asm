bb68130e25f52c53cd87216f16dbd76a77c40970
Dave Kilian <dave.kilian@gmail.com>
2022-11-28 14:49:00 -0800

    fibonacci

--- scratch.asm
+++ scratch.asm
@@ -1,20 +1,24 @@
 
 section .data
 
-    msg:    db  "Hello, world!", 10, 0
-    .len:   equ $ - msg
-
 section .text
     align 8
 
     global _main
 
 _main:
-    mov     rax, 0x2000004  ; write
-    mov     rdi, 1          ; fd = stdout
-    mov     rsi, msg
-    mov     rdx, msg.len
-    syscall
+
+    mov     rax, 1
+    mov     rdx, 1
+    mov     rcx, 2
+
+fib:
+    mov     r8, rax
+    add     rax, rdx
+    mov     rdx, r8
+    inc     rcx
+    cmp     rcx,20
+    jne     fib
 
     mov     rax, 0
     ret
